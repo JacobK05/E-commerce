2da713a4e804f365d2cef9e96f7a5ed3295fab91
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-04-17 17:06:29 -0500

    added the vite react library

---
+++ vite-project/src/components/header.jsx
@@ -0,0 +1,16 @@
+
+export default function header () {
+    return (
+        <>
+    <div>
+        <h1> E-commerce</h1>
+       <a> Shop </a> 
+        <a> Collections </a>
+        <a> Cart </a>
+     
+    </div>
+        
+        
+        </>
+    )
+}
