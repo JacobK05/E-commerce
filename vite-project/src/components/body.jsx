47213aeb40d6366e0f5689ebe5f571075d5f48e6
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-05-06 11:06:39 -0500

    changes some styling and add contact inputs

--- vite-project/src/components/body.jsx
+++ vite-project/src/components/body.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './style.css'
+import { Link } from 'react-router-dom'
+
 export default function body() {
   return (
     
     <div className='body-container'>
       <h1 className='body-h1'> Welcome </h1>
-      <p className='body-p'> This Store is a e-commerce clone made with react! </p>
-      <button className='body-btn'> Shop</button>
+      <p className='body-p'> This is a e-commerce store made with react! </p>
+      <Link to="/shop">
+      <button onClick  className='body-btn'> Shop</button>
+      </Link>
     </div>
   )
 }
--- vite-project/src/pages/contact.jsx
+++ vite-project/src/pages/contact.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import './style.css'
 export default function contact() {
   return (
-    <div>contact</div>
+    <>
+    <div className='contact-container'> 
+    <input className='name-input' type="text" placeholder='Name'/> 
+    <input className='email-input' type="text" placeholder='Email'/> 
+    <input className='message-input' type="text" placeholder='Message'/> 
+    <button className='contact-btn'> Submit </button>
+    </div>
+    </>
   )
 }
