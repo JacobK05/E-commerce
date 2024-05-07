64f6ba986568c391d8605ffe11df51fb38154b01
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-05-07 14:27:28 -0500

    fixed login error

--- vite-project/client/src/pages/login.jsx
+++ vite-project/client/src/pages/login.jsx
@@ -1,86 +1,94 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import { useMutation } from '@apollo/client'
-import { LOGIN_USER } from '../utils/mutations'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from '../utils/auth'
+import Auth from '../utils/auth';
 
-export default function login(props) {
-  const [formState, setFormState] = useState({ email: '', password: ''});
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
-
-
+ 
+  // update state based on form input changes
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setFormState({
       ...formState,
       [name]: value,
-    })
-  }
+    });
+  };
 
+  // submit form
   const handleFormSubmit = async (event) => {
-    event.preventDefault()
-    console.log(formState)
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
 
-    try{
-      const {data} = await login({
-        variables: {...formState}
-      })
-      Auth.login(data.login.token)
-    }catch (e) {
-      console.error(e)
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
 
+    // clear form values
     setFormState({
       email: '',
-      password: ''
-    })
-  }
+      password: '',
+    });
+  };
 
   return (
-    <main className='login'>
-      <div className='login-container'>
-        <h4 className='login-h4'> Login </h4>
-        <div className='login-body'>
-          {data ? (
-            <p>
-              You are now logged in now head {''}
-              <Link to="/"> back to the homepage.</Link>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-            </p>
-          ):(
-            <form onSubmit={handleFormSubmit}>
-              <input 
-                className='form-input'
-                placeholder='Email:'
-                name='email'
-                type='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input 
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='login-btn' style={{cursor: 'pointer'}} type='submit'>
-                Submit
-              </button>
-
-            </form>
-          )}
-          {error && (
-            <div className='error-msg'> {error.message}
-            </div>
-          )}
-
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error}
+              </div>
+            )}
+          </div>
         </div>
-
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Login;
--- vite-project/client/src/utils/mutations.js
+++ vite-project/client/src/utils/mutations.js
@@ -14,13 +14,12 @@ export const ADD_PROFILE = gql`
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password){
-        token
-        profile{
-            _id
-            name
-        }
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
     }
   }
-
-`
+`;
