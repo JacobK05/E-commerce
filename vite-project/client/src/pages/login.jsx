31bb1716483f03e9cdc7e892ad9e81cf0deff9c8
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-05-07 12:14:53 -0500

    finished the login.jsx file

--- vite-project/client/src/pages/login.jsx
+++ vite-project/client/src/pages/login.jsx
@@ -1,7 +1,86 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import { LOGIN_USER } from '../utils/mutations'
 
-export default function login() {
+import Auth from '../utils/auth'
+
+export default function login(props) {
+  const [formState, setFormState] = useState({ email: '', password: ''});
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formState)
+
+    try{
+      const {data} = await login({
+        variables: {...formState}
+      })
+      Auth.login(data.login.token)
+    }catch (e) {
+      console.error(e)
+    }
+
+    setFormState({
+      email: '',
+      password: ''
+    })
+  }
+
   return (
-    <>login</>
+    <main className='login'>
+      <div className='login-container'>
+        <h4 className='login-h4'> Login </h4>
+        <div className='login-body'>
+          {data ? (
+            <p>
+              You are now logged in now head {''}
+              <Link to="/"> back to the homepage.</Link>
+
+            </p>
+          ):(
+            <form onSubmit={handleFormSubmit}>
+              <input 
+                className='form-input'
+                placeholder='Email:'
+                name='email'
+                type='email'
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input 
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className='login-btn' style={{cursor: 'pointer'}} type='submit'>
+                Submit
+              </button>
+
+            </form>
+          )}
+          {error && (
+            <div className='error-msg'> {error.message}
+            </div>
+          )}
+
+        </div>
+
+      </div>
+    </main>
   )
 }
